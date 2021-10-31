00f681c46785f382a28b35e4613127da7bfe3e3c
brochj <brochj@gmail.com>
2021-10-31 11:08:24 -0300

    Add split splitOn and sentences functions

--- src/Analyze.hs
+++ src/Analyze.hs
@@ -26,10 +26,23 @@ lines = Prelude.lines
 numberOfLines :: String -> Int
 numberOfLines = length . Prelude.lines
 
-
 replace :: Char -> Char -> String -> String
 -- replace old new str = [if c == old then new else c | c <- str]
 replace old new  = map (\c -> if c == old then new else c)
 
--- sentences :: String -> [String]
--- sentences = filter . replace '?' '.'
+split :: Char -> String -> [String]
+split _ "" = []
+split delimiter str =
+    let (start, rest) = break (== delimiter) str
+        (_, remain) = span (== delimiter) rest -- this line removes the delimiter from rest
+     in start : split delimiter remain
+
+splitOn :: String -> String -> [String]
+splitOn _ "" = []
+splitOn delimiters str =
+    let (start, rest) = break (`elem` delimiters) str
+        (_, remain) = span (`elem` delimiters) rest -- this line removes the delimiter from rest
+     in start : splitOn delimiters remain
+
+sentences :: String -> [String]
+sentences = splitOn ".?!\n;" 
