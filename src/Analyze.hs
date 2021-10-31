bdb7199509081c44860cf80d841ba332ced0cf9a
brochj <brochj@gmail.com>
2021-10-31 16:19:09 -0300

    Add ARI index functions

--- src/Analyze.hs
+++ src/Analyze.hs
@@ -1,6 +1,7 @@
 module Analyze where
 import Data.List (nub)
 
+
 removePunctuation :: String -> String
 removePunctuation = filter (`notElem` chars)
     where chars = "!\"#$%&()*+,./:;<=>?@[\\]^_`{|}~0123456789"
@@ -17,8 +18,11 @@ words = uniqueWords . allWords . removePunctuation
 characters :: String -> Int
 characters = length
 
-nonSpaceCharacters :: String -> Int
-nonSpaceCharacters = length . filter (/=' ')
+nonSpaceCharacters :: String -> String
+nonSpaceCharacters = filter (/=' ')
+
+numberOfnonSpaceCharacters :: String -> Int
+numberOfnonSpaceCharacters = length . filter (/=' ')
 
 lines :: String -> [String]
 lines = Prelude.lines
@@ -45,4 +49,37 @@ splitOn delimiters str =
      in start : splitOn delimiters remain
 
 sentences :: String -> [String]
-sentences = splitOn ".?!\n;" 
+sentences = splitOn ".?!\n;"
+
+calculateARI :: String -> Int
+calculateARI text = ceiling (term1 + term2)
+   where
+       chars = fromIntegral $ numberOfnonSpaceCharacters text
+       numberOfWords = fromIntegral . length $ Analyze.words text
+       numberOfsentences = fromIntegral . length $ sentences text
+       term1 = 4.71 * ( chars / numberOfWords )
+       term2 = 0.5 * (numberOfWords / numberOfsentences) - 21.43
+
+meaningARI :: Int -> IO ()
+meaningARI indexARI = putStrLn $ findKey parameterizedARI meaning_string
+    where
+        parameterizedARI = if indexARI <= 14 then indexARI else 14
+        meaning_string =
+            [(1,"5-6 : Kindergarten")
+            ,(2,"6-7 : First/Second Grade")
+            ,(3,"7-9 : Third Grade")
+            ,(4,"9-10 : Fourth Grade")
+            ,(5,"10-11 : Fifth Grade")
+            ,(6,"11-12 : Sixth Grade")
+            ,(7,"12-13 : Seventh Grade")
+            ,(8,"13-14 : Eighth Grade")
+            ,(9,"14-15 : Ninth Grade")
+            ,(10,"15-16 : Tenth Grade")
+            ,(11,"16-17 : Eleventh Grade")
+            ,(12,"17-18 : Twelfth grade")
+            ,(13,"18-24 : College student")
+            ,(14,"24+ : Professor")
+            ]
+
+findKey :: (Eq k) => k -> [(k,v)] -> v
+findKey key = snd . head . filter (\(k,v) -> key == k)    
