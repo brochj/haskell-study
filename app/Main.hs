8a3a4e16a6a97a3d9d5eab2cb45b43e6a2a07d8c
brochj <brochj@gmail.com>
2021-10-30 21:45:30 -0300

    Add analyze module

--- app/Main.hs
+++ app/Main.hs
@@ -2,9 +2,9 @@ module Main where
 
 import qualified MyLib (someFunc)
 import qualified Baby 
+import qualified Analyze (words)
 
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
-  MyLib.someFunc
-  Baby.someFunc 
+  -- putStrLn "Hello, Haskell!! I'm from New York. New York is a beautiful city"
+  print $ Analyze.words "Hello, Haskell!! I'm from Ney York. \nNew York is a beautiful city"
